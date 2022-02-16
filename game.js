bedf64872d5d0027f4bd8f62171c689688a18410
joshraiborde <joshraiborde@gmail.com>
2022-02-16 13:57:58 -0500

    steps 6 & 7, added animation user clicks, start the game.

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
-var gamePattern = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
+var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var started = false;
+
 
 $(".btn").click(function() {
-    var userChosenColour = this.id;
-    userClickedPattern.push(userChosenColour)
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatedPress(userChosenColour);
+    nextSequence();
     
 });
 
@@ -20,9 +23,32 @@ function nextSequence() {
         $("." + randomChosenColour).fadeOut(100).fadeIn(100);
         playSound(randomChosenColour)
     })
+
 }
 
 function playSound(name) {
     var audio = new Audio('/sounds/' + name + ".mp3")
     audio.play();
+}
+
+
+function animatedPress(currentColour) {
+    $("." + currentColour).addClass("pressed")
+    setTimeout(() => {
+        $("." + currentColour).removeClass("pressed")
+    }, 100);
+    
+}
+
+$(document).one("keydown", function(event) {
+    // alert("first key: " + event.key);
+if (!started) {
+    $("#level-title").text("Level " + level)
+    nextSequence();
+    started = true;
+}
+})
+
+function checkAnswer(currentLevel) {
+    
 }
