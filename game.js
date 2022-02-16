cbf71d11f9d0f38604c34a6009ad93a47cf25a06
joshraiborde <joshraiborde@gmail.com>
2022-02-16 13:06:00 -0500

    Steps 3 to 5, show the sequence with animations and sound. button pressed. added sound to button clicks

--- game.js
+++ game.js
@@ -1,11 +1,28 @@
 var gamePattern = [];
 
-var buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour)
+    playSound(userChosenColour);
+    
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber
-}
-var randomChosenColor = buttonColors[nextSequence()];
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
-gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColour).on("click", function(){
+        $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+        playSound(randomChosenColour)
+    })
+}
+
+function playSound(name) {
+    var audio = new Audio('/sounds/' + name + ".mp3")
+    audio.play();
+}
